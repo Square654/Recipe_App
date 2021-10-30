1babc28c330853144cbdf73aeff30a611734a51c
Yanji Tan <yanjitan1204@gmail.com>
2021-10-30 21:38:49 +0800

    -Added RecipeFeaturedView -Added the EnvionmentObject modifier -Created an instance of RecipeModel in the RecipeTabView

---
+++ recipe_list_app/View/RecipeFeaturedView.swift
@@ -0,0 +1,23 @@
+//
+//  RecipeFeaturedView.swift
+//  recipe_list_app
+//
+//  Created by Yanji Tan on 10/30/21.
+//
+
+import SwiftUI
+
+struct RecipeFeaturedView: View {
+    
+    @EnvironmentObject var model:RecipeModel
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecipeFeaturedView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeFeaturedView()
+    }
+}
